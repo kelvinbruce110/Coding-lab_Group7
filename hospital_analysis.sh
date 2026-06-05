9fef4cdaf940d95ebd5c8dc4c4afb37481653438
kelvinbruce110 <b.mupenzi1@alustudent.com>
2026-06-05 05:17:07 -0700

    Format critical alerts and generate report

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -6,6 +6,11 @@ process_vitals() {
     echo "Generating critical alerts report..."
 
     grep "CRITICAL" active_logs/heart_rate.log active_logs/temperature.log | \
+    awk -F'|' '
+{
+    printf "Timestamp: %s | Device_ID: %s | Value: %s\n",
+    $1, $2, $3
+}' > reports/critical_alerts.txt
 
 }
 
