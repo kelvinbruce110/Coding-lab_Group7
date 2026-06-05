95fe118043421af84c3937a9b0a1b9971b526b29
kelvinbruce110 <b.mupenzi1@alustudent.com>
2026-06-05 05:12:03 -0700

    Add critical alert filtering from log files

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -3,7 +3,9 @@
 #Clinical Analyst (member 5)
 
 process_vitals() {
+    echo "Generating critical alerts report..."
 
+    grep "CRITICAL" active_logs/heart_rate.log active_logs/temperature.log | \
 
 }
 
